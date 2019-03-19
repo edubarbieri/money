e5c0b90366ec666ea422f54293f501235c9cf574
Eduardo Barbieri <duduardo23@gmail.com>
2019-03-18 22:17:06 -0300

    Teste Deploy

--- packages/backend/src/env.js
+++ packages/backend/src/env.js
@@ -3,6 +3,6 @@ module.exports = {
 	authSecret: process.env.AUTH_SECRET || 'd4d11e98cbbcb84e773b8b3328627267',
 	staticPath : process.env.STATIC || '../public',
 	mysqlURL : process.env.MYSQL_URL || 'mysql://mymoney:mymoney@localhost:3306/mymoney',
-	postgresURL : process.env.POSTGRES_URL || 'postgres://mymoney:mymoney@localhost:5432/mymoney',
+	postgresURL : process.env.DATABASE_URL || 'postgres://mymoney:mymoney@localhost:5432/mymoney',
 	defaultPageSize: 10
 };
