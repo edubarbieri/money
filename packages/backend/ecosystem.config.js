b84db221b2ffc49d5589dd0716f5c6e360547063
Eduardo dos Santos Barbieri <eduardo.santos@compasso.com.br>
2019-04-26 15:41:33 -0300

    Change to 2 instances

--- packages/backend/ecosystem.config.js
+++ packages/backend/ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps : [{
     name: 'MeuDinheiro backend api',
     script: './src/index.js',
-    instances: "max",
+    instances: 2,
     autorestart: true,
     watch: false,
     max_memory_restart: '1G',
