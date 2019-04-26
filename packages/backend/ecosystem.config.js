2ed07920a2c4f42057b2efeab42fdb67137b8401
Eduardo dos Santos Barbieri <eduardo.santos@compasso.com.br>
2019-04-26 15:43:28 -0300

    Change use memory

--- packages/backend/ecosystem.config.js
+++ packages/backend/ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
     instances: 2,
     autorestart: true,
     watch: false,
-    max_memory_restart: '1G',
+    max_memory_restart: '250M',
     env: {
       NODE_ENV: 'development'
     },
