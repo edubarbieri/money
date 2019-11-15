5e429826c8368b6cf177db821889136557015a49
Matheus Barbieri <mdsbarbieri@gmail.com>
2019-11-15 18:34:27 -0300

    Add possibilite to change date on resume cart

--- packages/web/src/components/cards/TotalBillsCard.jsx
+++ packages/web/src/components/cards/TotalBillsCard.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import 'moment/locale/pt-br';
 import { fetchTotalBills } from 'reducers/bills/billsAction';
@@ -7,6 +7,7 @@ import moment from 'moment';
 import { getLang } from 'i18n/lang';
 import { formatCurrency } from 'services/Util';
 import RegisterLink from 'components/global/RegisterLink';
+import {toPattern} from 'vanilla-masker';
 
 moment.locale(getLang().toLowerCase());
 
@@ -14,14 +15,42 @@ const TotalBillsCard = () => {
     const dispatch = useDispatch();
     const monthResume = useSelector(state => state.bills.totalMonth);
     const refresh = useSelector(state => state.global.refresh);
-
+    
     const year = moment().get('year');
     const month = moment().month();
-    const [filter] = useState({
+    const [filter, setFilter] = useState({
         year: year,
         month: month + 1
     });
+    
+    const [expanded, setExpanded] = useState(false);
+    const [date, setDate] = useState(filter.month +'/'+ filter.year);
 
+    const setDateFilter = (event) => {
+        let value = event.target.value;
+        if(value.length === 2){
+            value =  toPattern(value, '99/9999');
+        }
+        setDate(value);
+    }
+
+
+    const changeFilterSubmit = (event) => {
+        event.preventDefault();
+        changeFilter();
+    }
+
+    const changeFilter = () => {
+        const submitDate =  toPattern(date, '99/9999');
+        if(!moment(submitDate, 'MM/YYYY').isValid()){
+            return;
+        }
+        const [month, year] = submitDate.split('/');
+        setFilter({month: Number(month), year: Number(year)});
+        setExpanded(false);
+        setDate(submitDate);
+    }
+    
     useEffect(() => {
         dispatch(fetchTotalBills(filter));
     }, [dispatch, filter, refresh]);
@@ -31,9 +60,17 @@ const TotalBillsCard = () => {
             <div className="card content card-month-resume">
                 <div className="card-body p-1">
                     <div className="d-flex">
-                        <div className="month-info flex-column ">
+                        <div className="month-info flex-column" onClick={() => setExpanded(true)}>
                             <span className="text-danger">{bundle('bills')}</span>
-                            <span>{filter.month}/{filter.year}</span>
+                            {!expanded && <span>{filter.month}/{filter.year}</span>}
+                            {expanded && <form onSubmit={changeFilterSubmit} className="text-center">
+                                <input type="text" 
+                                    autoFocus 
+                                    className="input-wizard"
+                                    value={date}
+                                    onBlur={changeFilter} 
+                                    onChange={setDateFilter}/>
+                            </form>}
                         </div>
                     </div>
                     <div className=" d-flex flex-column resume">
--- packages/web/src/components/cards/TotalExtractCard.jsx
+++ packages/web/src/components/cards/TotalExtractCard.jsx
@@ -8,6 +8,7 @@ import { formatCurrency } from 'services/Util';
 import RegisterLink from 'components/global/RegisterLink';
 import { fetchTotalDebits } from 'reducers/debit/debitAction';
 import { fetchTotalCredits } from 'reducers/credit/creditAction';
+import {toPattern} from 'vanilla-masker';
 
 moment.locale(getLang().toLowerCase());
 
@@ -19,7 +20,7 @@ const TotalExtractCard = () => {
 
     const year = moment().get('year');
     const month = moment().month();
-    const [filter] = useState({
+    const [filter, setFilter] = useState({
         year: year,
         month: month + 1
     });
@@ -28,6 +29,33 @@ const TotalExtractCard = () => {
         dispatch(fetchTotalCredits(filter));
         dispatch(fetchTotalDebits(filter));
     }, [dispatch, filter, refresh]);
+
+    const [expanded, setExpanded] = useState(false);
+    const [date, setDate] = useState(filter.month +'/'+ filter.year);
+
+    const setDateFilter = (event) => {
+        let value = event.target.value;
+        if(value.length === 2){
+            value =  toPattern(value, '99/9999');
+        }
+        setDate(value);
+    }
+
+    const changeFilterSubmit = (event) => {
+        event.preventDefault();
+        changeFilter();
+    }
+
+    const changeFilter = () => {
+        const submitDate =  toPattern(date, '99/9999');
+        if(!moment(submitDate, 'MM/YYYY').isValid()){
+            return;
+        }
+        const [month, year] = submitDate.split('/');
+        setFilter({month: Number(month), year: Number(year)});
+        setExpanded(false);
+        setDate(submitDate);
+    }
 
     const getBalance = () => {
         const diff = debitMonth.amount - creditMonth.amount;
@@ -42,9 +70,17 @@ const TotalExtractCard = () => {
             <div className="card content card-month-resume extract">
                 <div className="card-body p-1">
                     <div className="d-flex">
-                        <div className="month-info flex-column ">
+                        <div className="month-info flex-column" onClick={() => setExpanded(true)}>
                             <span className="text-primary">{bundle('extract')}</span>
-                            <span>{filter.month}/{filter.year}</span>
+                            {!expanded && <span>{filter.month}/{filter.year}</span>}
+                            {expanded && <form onSubmit={changeFilterSubmit} className="text-center">
+                                <input type="text" 
+                                    autoFocus 
+                                    className="input-wizard"
+                                    value={date}
+                                    onBlur={changeFilter} 
+                                    onChange={setDateFilter}/>
+                            </form>}
                         </div>
                     </div>
                     <div className=" d-flex flex-column resume">
